33459da6635edfb19fb2f41108ea38d4c09afe73
neha <neha.khan1@ontariotechu.net>
2025-11-15 20:31:32 -0500

    fix for axios error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an isolated axios instance that ALWAYS uses Django's URL
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://127.0.0.1:8000",
 });
 
 // --- REQUEST INTERCEPTOR ---
